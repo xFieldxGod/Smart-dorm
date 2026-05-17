59fd18ad499eb30225f1ebe792749100bcda8949
xFieldxGod <xfieldxgod@gmail.com>
2026-05-18 00:33:25 +0700

    update backend url to render

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://smart-dorm.onrender.com",
         changeOrigin: true,
       },
     },
